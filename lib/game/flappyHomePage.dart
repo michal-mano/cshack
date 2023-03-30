645460e575911292fd4923372693fe220777bfa6
roeez <roeez55@gmail.com>
2023-03-30 17:24:44 +0300

    game v2

--- lib/game/flappyHomePage.dart
+++ lib/game/flappyHomePage.dart
@@ -11,15 +11,31 @@ class flappyHomePage extends StatefulWidget {
 }
 
 class _flappyHomePageState extends State<flappyHomePage> {
+  static double birdYaxis = 0;
+  double time = 0;
+  double height = 0;
+  double initialHeight = birdYaxis;
+  bool gameHasStarted = false;
 
-  double birdYaxis = 0;
+  void jump() {
+    setState(() {
+      time = 0;
+      initialHeight = birdYaxis;
+    });
+  }
 
-
-  void jump(){
-    Timer.periodic(Duration(milliseconds: 100), (timer) {
+  void startGame() {
+    gameHasStarted = true;
+    Timer.periodic(Duration(milliseconds: 50), (timer) {
+      time += 0.05;
+      height = -4.9 * time * time + 2.8 * time; //2.8 is initial y velocity
       setState(() {
-        birdYaxis -= 1;
+        birdYaxis = initialHeight - height;
       });
+      if (birdYaxis > 1) {
+        timer.cancel();
+        gameHasStarted = false;
+      }
     });
   }
 
@@ -31,18 +47,49 @@ class _flappyHomePageState extends State<flappyHomePage> {
         Expanded(
           flex: 2,
           child: GestureDetector(
-            onTap: jump,
+            onTap: () {
+              if (gameHasStarted) {
+                jump();
+              } else {
+                startGame();
+              }
+            },
             child: AnimatedContainer(
-              alignment: Alignment(0,birdYaxis),
+              alignment: Alignment(0, birdYaxis),
               duration: Duration(milliseconds: 0),
               color: Colors.blue,
               child: MyBird(),
             ),
           ),
         ),
+        Container(
+          height: 15,
+          color: Colors.green,
+        ),
         Expanded(
           child: Container(
-            color: Colors.green,
+            color: Colors.brown,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Text("SCORE", style: TextStyle(color: Colors.white, fontSize: 20)),
+                    SizedBox(height: 20
+                      ,),
+                    Text("0", style: TextStyle(color: Colors.white, fontSize: 35)),
+                  ],
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Text("BEST", style: TextStyle(color: Colors.white, fontSize: 20)),
+                    Text("10", style: TextStyle(color: Colors.white, fontSize: 35)),
+                  ],
+                )
+              ],
+            ),
           ),
         ),
       ],
